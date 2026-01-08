cb2a3d660de56a8937e0e6cdc3037315b78717e3
Hetvi Patel <hetvipatel2302@gmail.com>
2026-01-08 17:08:26 +0000

    Add hooks.py wrapper to expose crm.hooks as crm_dinematters.hooks

---
+++ crm_dinematters/hooks.py
@@ -0,0 +1,4 @@
+# This is a wrapper hooks file for bench compatibility
+# The actual hooks are in 'crm.hooks'
+# Import everything from crm.hooks to make it available as crm_dinematters.hooks
+from crm.hooks import *
