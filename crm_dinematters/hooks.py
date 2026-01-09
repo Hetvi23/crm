9dcc82f507c7e5a992803dc7cb1711ecc0b37660
Hetvi Patel <hetvipatel2302@gmail.com>
2026-01-09 05:02:25 +0000

    Override app_name to crm_dinematters in wrapper hooks.py

--- crm_dinematters/hooks.py
+++ crm_dinematters/hooks.py
@@ -2,3 +2,7 @@
 # The actual hooks are in 'crm.hooks'
 # Import everything from crm.hooks to make it available as crm_dinematters.hooks
 from crm.hooks import *
+
+# Override app_name to match the installed app name (crm_dinematters)
+# This is necessary for bench to correctly identify the app during installation
+app_name = "crm_dinematters"
